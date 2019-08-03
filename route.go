508bca3a4a68822fdddd5fa04afa22b27578cb54
Kristian Lopez <kristian.lom@gmail.com>
2019-08-03 01:10:36 -0500

    Uso de parametros

--- route.go
+++ route.go
@@ -13,4 +13,5 @@ func startRoutes(e *echo.Echo) {
 	e.POST("/api/v1/users", usuario.Create)
 	e.GET("/api/v1/users", usuario.GetAll)
 	e.GET("/api/v1/users/:email", usuario.GetByEmail)
+	e.GET("api/v1/users-paginate", usuario.GetAllPaginate)
 }
